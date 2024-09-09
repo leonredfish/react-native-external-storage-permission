7be5fefc2c1c759d2a585fd61fe63d66fa9dc8a2
Leon Li <leon_redfish@163.com>
2024-09-09 20:04:42 +0800

    delete all useless files

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  ManageExternalStorageExample
-//
-
-import Foundation
--- ios/ManageExternalStorage.swift
+++
@@ -1,8 +0,0 @@
-@objc(ManageExternalStorage)
-class ManageExternalStorage: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
